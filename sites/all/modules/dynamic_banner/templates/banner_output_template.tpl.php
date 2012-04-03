98c5153da7b824f6cc001626683653fc30505535
An Dong <andong135@126.com>
2012-04-03 10:03:02 +0800

    nihao

---
+++ sites/all/modules/dynamic_banner/templates/banner_output_template.tpl.php
@@ -0,0 +1,73 @@
+<?php
+
+  // $Id: banner_output_template.tpl.php,v 1.2 2010/09/30 14:38:03 coolestdude1 Exp $
+
+  /**
+   * @file
+   * This file will determine how the dynamic banner block will display
+   */
+
+  // input variables need to have the same name as the theme definitions
+
+  // Url
+  $url;
+  // Text (optional)
+  $text;
+  // Link (optional)
+  $link;
+  // display setting
+  $display_setting;
+  // display errors
+  $display_errors;
+
+  // default the output
+  $output = "";
+
+  // decide how to output based on what the displaysetting is
+  if ($display_setting == 'urltext') {
+    // check for variables
+    if ($url && $text) {
+      // form the html snippet and print
+      $output .= "<div id='banner-left'>";// match this up with your css
+      $output .= "<img src='/$url' class='jpg' alt='banner' width='596' height='209' />";
+      $output .= "</div><div id='banner-right'>";// if you have text in your banner match this up with your css
+      $output .= "<p id='bannerTitle'>" . $text . "</p>";
+      $output .= "</div>";
+    }
+    elseif ( $display_errors) {
+      $output .= "banner problem";
+    }
+  }
+  elseif ($display_setting == 'url') {
+    // just an image url came in
+    if ($url) {
+      $output .= "<div id='banner'>";
+      $output .= "<img src='/nihao/$url' class='jpg' alt='banner' width='764' height='280' />";
+      $output .= "</div>";
+    }
+    elseif ( $display_errors) {
+      $output .= "banner problem";
+    }
+  }
+  elseif ($display_setting == 'text') {
+    // just text came in
+    if ($url) {
+      $output .= "<div id='banner'>";
+      $output .= "<p>$text</p>";
+      $output .= "</div>";
+    }
+    elseif ( $display_errors) {
+      $output .= "banner problem";
+    }
+  }
+  elseif ($display_setting == 'urllink') {
+	if ($url && $link) {
+		$output .= "<div id='banner'>";
+		$output .= "<a class='link' href='/$link'><img class='jpg' alt='banner' src='/$url'></a>";
+		$output .= "</div>";
+	}
+	elseif ($display_errors) {
+        $output .= "banner problem";
+	}
+  }
+  print $output;
